71fd526f91b0a9550fb101b76e8e90cabe6c3165
Federico Tensi <federicotensi@gmail.com>
2022-11-01 16:21:48 +0100

    Basic game logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,94 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+
+const getRandomHEXColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`
+}
+
+enum Result {
+  Correct,
+  Wrong,
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [color, setColor] = useState<string>('');
+  const [answers, setAnswers] = useState<string[]>([]);
+  const [result, setResult] = useState<Result | undefined>(undefined);
+
+  const [score, setScore] = useState<number>(0);
+  const [life, setLife] = useState<number>(3);
+
+
+  const generateColors = () => {
+    const corretAnswer = getRandomHEXColor();
+    setColor(corretAnswer)
+    setAnswers([corretAnswer, getRandomHEXColor(), getRandomHEXColor()].sort(() => Math.random() - 0.5))
+    setResult(undefined)
+    
+  }
+
+
+
+  useEffect(() => {
+    generateColors();
+  }, []);
+
+  const handleAnswerClick = (answer: string) => {
+    if (answer === color) {
+      
+      setResult(Result.Correct)
+      generateColors();
+
+    } else {
+      setResult(Result.Wrong)
+    }
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <h1><span style={{'color': color}}>HEX</span> color guessing</h1>
+        <h3>
+          {result === Result.Wrong && <>Wrong one!</>}
+          {result === Result.Correct && <> You <span style={{'color': color}}>guessed </span> it!</>}
+          {result === undefined && <>Which one?</>}
+        </h3>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      
+      <div className="color-box"
+      style={{
+        backgroundColor: color,
+        boxShadow: `0px 0px 50px 13px ${color}`
+      }}>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="buttons-container">
+        {
+          answers.map((answer, index) => (
+            <button 
+            onClick={() => handleAnswerClick(answer)} 
+            key={index}>{answer}</button>
+          ))
+        }
+        
+      </div>
+      <div className="game-info">
+        <div className="score">
+          <h3>Score</h3>
+          <p>{score}</p>
+          </div>
+          <div className="life">
+          <h3>Life</h3>
+          <p>{life}</p>
+          </div>
+      </div>
+     
+     
     </div>
   )
 }
